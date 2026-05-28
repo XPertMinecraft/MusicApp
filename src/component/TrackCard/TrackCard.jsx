d474db87d49fbb9ae397a8f4acbe478053d894f8
Максик <maksimzitcenko@gmail.com>
2026-05-28 19:18:10 +0300

    Все страницы уже готовы

--- src/component/TrackCard/TrackCard.jsx
+++ src/component/TrackCard/TrackCard.jsx
@@ -1,43 +1,62 @@
-//Я руки вырву тому кто классы писал
 import './TrackCard.css'
 import { useContext } from "react";
 import { PlayerContext } from "../../context/PlayerContext";
-//В функцию ложим props что б пользоваться им
+
 export default function TrackCard(props) {
-   
+
+    const { selectTrack, addToFav, removeFromFav, favorites } =
+        useContext(PlayerContext);
+
+    const isFav = favorites.some(t => t.id === props.id);
+
     return (
-        
         <section className="trackcard">
+
             <p className="num">1</p>
-<img className='iii' src="https://cdn-icons-png.flaticon.com/512/727/727245.png" alt="" />            <div className="name">
-                {/* Выберу это как пример. Тут я беру props и
-                с его помощью беру свойсвтво что мне нужно
-                из Home. Props это объект, не забывайте*/}
+
+            <img
+                className='iii'
+                src="https://cdn-icons-png.flaticon.com/512/727/727245.png"
+                alt=""
+            />
+
+            <div className="name">
                 <h4 className='hhh4'>{props.title}</h4>
                 <p className='num'>{props.album}</p>
             </div>
- <p className='num1'>{props.title}</p>
-            <p className='num12'>2:37</p>
-            {/* Тут я объясню что с каверами делать. Как я понял
-            у каверов должна быть приписка "кавер", но в объект
-            проще засунуть true/false и потом отобразить правильно
-            уже здесь. Работает всё на тернарном операторе (if/else
-            в одну строку). Если свойсвтво cover переданное из
-            Home совпадает с true, то написать "cover", иначе
-            не писать ничего. Всё */}
+
             <p className="isCover">
-                {props.cover===true?"Cover":""}
+                {props.cover ? "Cover" : ""}
             </p>
+
             <button onClick={() => selectTrack({
-    id: props.id,
-    title: props.title,
-    album: props.album,
-    cover: props.cover,
-    src: props.src
-})}>
-    ▶ Play
-</button>
+                id: props.id,
+                title: props.title,
+                album: props.album,
+                cover: props.cover,
+                src: props.src
+            })}>
+                ▶ Play
+            </button>
+
+            <button
+                onClick={() => {
+                    if (isFav) {
+                        removeFromFav(props.id);
+                    } else {
+                        addToFav({
+                            id: props.id,
+                            title: props.title,
+                            album: props.album,
+                            cover: props.cover,
+                            src: props.src
+                        });
+                    }
+                }}
+            >
+                {isFav ? "💖" : "🤍"}
+            </button>
+
         </section>
     )
 }
-//На этом с вами прощаюсь. Это мой последний коммит в этой работе
